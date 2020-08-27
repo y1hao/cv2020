f0533049e988b70a2b6634e33070f1c79be5fa41
CoderYihaoWang <yesiamyihao@gmail.com>
2020-08-27 19:18:27 +1200

    Updates

---
+++ precache-manifest.466eee9dc68699cb3fc213213a3669f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded731fb20812795f102e58d49c4fa12",
+    "url": "/cv2020/index.html"
+  },
+  {
+    "revision": "f9c45da9719b8c73493d",
+    "url": "/cv2020/static/css/main.3c6b70e3.chunk.css"
+  },
+  {
+    "revision": "f6be8a65bfbf102b052a",
+    "url": "/cv2020/static/js/2.5973b98a.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/cv2020/static/js/2.5973b98a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9c45da9719b8c73493d",
+    "url": "/cv2020/static/js/main.ef8774dc.chunk.js"
+  },
+  {
+    "revision": "8df12c586231c6992bbd",
+    "url": "/cv2020/static/js/runtime-main.638f6ec5.js"
+  },
+  {
+    "revision": "10790a223250ca157725d7c4ec6ed15b",
+    "url": "/cv2020/static/media/KeyPoint.10790a22.png"
+  },
+  {
+    "revision": "119a24ebe67a64df20da87119a800f54",
+    "url": "/cv2020/static/media/avatar.119a24eb.jpg"
+  },
+  {
+    "revision": "1b1cceb2123ea1a9e226d26ab028d846",
+    "url": "/cv2020/static/media/game.1b1cceb2.png"
+  }
+]);
