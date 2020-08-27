59e8fe5d1c1abe580806efe533c4fd215f48d1a3
CoderYihaoWang <yesiamyihao@gmail.com>
2020-08-27 17:00:33 +1200

    Add emphasis and links

--- src/Components/Content.tsx
+++ src/Components/Content.tsx
@@ -278,7 +278,7 @@ export default function Content() {
                 So, in the last two trimesters of program, I started seriously learning programming on my own. I got myself familiar with 
                 <span className={classes.emphasis}> HTML </span>, <span className={classes.emphasis}> CSS </span> and <span className={classes.emphasis}> JavaScript </span>. 
                 I studied through K&R's awesome book <span className={classes.emphasis}> The C Programming Language </span>. I also took a wonderful course 
-                on Coursera on computer architecture and compilers - <a className={classes.link} href="https://coursera.org/share/bf6bd6231e4178c5aaff434b592c063b" target="_blank"> From Nand to Tetris </a>, 
+                on Coursera on computer architecture and compilers - <a className={classes.link} href="https://coursera.org/share/bf6bd6231e4178c5aaff434b592c063b" target="_blank"> Nand to Tetris </a>, 
                 which is one of the top courses on that platform.
               </div>
             </div>
@@ -345,10 +345,12 @@ export default function Content() {
           <Fade>
             <div className={classes.contentWrapper}>
               <div className={classes.description}>
-                This is the project that I built along the course Nand to Tetris.
+                <br />
+                This is the project that I built along the course <a className={classes.link} href="https://coursera.org/share/bf6bd6231e4178c5aaff434b592c063b" target="_blank">Nand to Tetris</a>.
                 It consists of 12 sub-projects, which, when fitted together, make a complete computer built from scratch.
-                These include logic gates, the ALU and CPU, the machine language, the assembly language and the assembler, the virtual machine, 
-                the high-level language and its compiler, the OS and finally a game that runs on this computer.
+                These include <span className={classes.emphasis}> logic gates </span>, the <span className={classes.emphasis}> ALU </span> and <span className={classes.emphasis}> CPU </span>, the <span className={classes.emphasis}> machine language </span>, 
+                the <span className={classes.emphasis}> assembly language </span> and the <span className={classes.emphasis}> assembler </span>, the <span className={classes.emphasis}> virtual machine </span>, 
+                the <span className={classes.emphasis}> high-level language </span> and its <span className={classes.emphasis}> compiler </span>, the <span className={classes.emphasis}> OS </span> and finally a <span className={classes.emphasis}> game </span> that runs on this computer.
                 <br /><br />
                 I built this during my master's degree. It gave me good understanding of many fundamental concepts in computer science, as well as 
                 encouraged me to explore more.
@@ -360,8 +362,8 @@ export default function Content() {
           <Fade>
             <div className={classes.contentWrapper}>
               <div className={classes.description}>
-                <br />
-                This is the final part of this project - an RPG game called Magic Tower. 
+                <br /><br />
+                This is the final part of this project - an RPG game called <span className={classes.emphasis}> Magic Tower </span>. 
                 <br />
                 Despite it looks primitive, it was built completely from scratch - I mean, from hardware logic, to machine language,
                 to assembly language, to virtual machine and the high level language which runs on it, to the pixel-level rendering on the screen, 
@@ -369,7 +371,7 @@ export default function Content() {
               </div>
               <iframe className={classes.media} frameBorder={0} src="https://www.youtube.com/embed/03VlOt-UVdA" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; full-screen" allowFullScreen></iframe>
               <div className={classes.description}>
-                You can have a look at the repository, and know more about this project on its official site.
+                You can have a look at the <a className={classes.link} href="https://github.com/CoderYihaoWang/Nand2Tetris" target="_blank"> source code </a>, or know more about this project on its <a className={classes.link} target="_blank" href="https://www.nand2tetris.org/"> official site </a>.
               </div>
             </div>
           </Fade>
@@ -391,14 +393,15 @@ export default function Content() {
           <Fade up>
             <div className={classes.contentWrapper}>
               <div className={classes.description}>
-                div is the interpreter of a minimal programming language, written in only 500 lines of JavaScript code.
+                <a className={classes.link} target="_blank" href="https://coderyihaowang.github.io/Div/"> div </a> is the <span className={classes.emphasis}> interpreter </span> of a minimal programming language, written in only 500 lines of <span className={classes.emphasis}> JavaScript </span> code.
                 It is an experimental project which tried to answer the popular question: "is HTML a programming language?"
-                My answer to it is, yes, if you write an interpreter for it. 
-                <br />
-                Any valid div code is also a valid HTML5 file (syntax-wise). You can insert this piece of code 
+                My answer to it is: yes, if you write an interpreter for it. 
+                <br /><br />
+                Any valid div code is also a valid HTML5 file (syntax-wise). You can insert this interpreter
                 using the &lt;script&gt; tag on a HTML5 file. And then it will treat the HTML elements on the page as the syntax tree.
-                The end result is a Lisp-like functional programming language, which supports recursion and closure. It is 
-                theoretically Turing-complete. You can have a look at the source code, the language specification and some demos here.
+                The end result is a Lisp-like <span className={classes.emphasis}> functional programming language </span>, which supports <span className={classes.emphasis}> recursion </span>, <span className={classes.emphasis}> first-class function </span> and <span className={classes.emphasis}> closure </span>. It is 
+                theoretically <span className={classes.emphasis}> Turing-complete </span>. You can have a look at the <a className={classes.link} target="_blank" href="https://github.com/CoderYihaoWang/Div"> source code and the language specification </a>,
+                 and <a className={classes.link} target="_blank" href="https://coderyihaowang.github.io/Div/"> here </a> are some demos.
               </div>
             </div>
           </Fade>
@@ -420,13 +423,14 @@ export default function Content() {
           <Fade up>
             <div className={classes.contentWrapper}>
               <div className={classes.description}>
-                KeyPoint is a blogging website. This is the final project of the PGCertIT program.
-                It used MariaDB for the database, JSP and Servlet for the backend, HTML, CSS, JavaScript, Bootstrap and jQuery for the frontend, 
-                and git and GitLab for version control and cooperation.
+                <a className={classes.link} target="_blank" href="https://trex-sandwich.com/taz_KeyPoint/ "> KeyPoint</a> is a blogging website. This is the final project of the PGCertIT program.
+                It used <span className={classes.emphasis}> MariaDB </span> for the database, <span className={classes.emphasis}> JSP </span> and <span className={classes.emphasis}> Servlet </span> for the backend, 
+                <span className={classes.emphasis}> HTML </span>, <span className={classes.emphasis}> CSS </span>, <span className={classes.emphasis}> JavaScript </span>, <span className={classes.emphasis}> Bootstrap </span> and <span className={classes.emphasis}> jQuery </span> for the frontend, 
+                and <span className={classes.emphasis}> Git </span> and <span className={classes.emphasis}> GitLab </span> for version control and teamwork.
               </div>
               <img className={classes.media} src={keyPoint}></img>
               <div className={classes.description}>
-                You can find the source code and more detailed description in this repository.
+                You can find the source code and more detailed description in <a className={classes.link} target="_blank" href="https://github.com/CoderYihaoWang/KeyPoint"> this repository </a>.
               </div>
             </div>
           </Fade>
@@ -448,14 +452,14 @@ export default function Content() {
           <Fade up>
             <div className={classes.contentWrapper}>
               <div className={classes.description}>
-                Colorful Life is a zero-player game based on Conway's Game of Life. 
+                <a className={classes.link} target="_blank" href="https://coderyihaowang.github.io/ColorfulLife/">Colorful Life </a> is a zero-player game based on <a className={classes.link} target="_blank" href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"> Conway's Game of Life </a>. 
                 It is different from the original game in that in this game, everything is configurable, including all game veriables and styles of the cells. 
               </div>
               <img className={classes.media} src={game}></img>
               <div className={classes.description}>
-                I used TypeScript, React and Material-UI for building this project. The project can be used as a kind of generative art. 
+                I used <span className={classes.emphasis}> TypeScript </span>, <span className={classes.emphasis}> React </span> and <span className={classes.emphasis}> Material-UI </span> for building this project. The project can be used as a kind of generative art. 
                 You can play with it to achieve various visual effects. 
-                You can try it on this deployed version, or have look at the source code and some interesting examples.
+                You can try it on this <a className={classes.link} target="_blank" href="https://coderyihaowang.github.io/ColorfulLife/"> deployed version </a>, or have look at the <a className={classes.link} target="_blank" href="https://github.com/CoderYihaoWang/ColorfulLife"> source code and some interesting examples </a>.
               </div>
             </div>
           </Fade>
@@ -469,11 +473,12 @@ export default function Content() {
           <Fade up>
             <div className={classes.contentWrapper}>
               <div className={classes.description}>
-                I am always learning something. At the moment, I am exploring EF Core and building a project with EF Core, .Net Core and React.
+                I am always learning something. At the moment, I am exploring <span className={classes.emphasis}> EF Core </span> and building a project with <span className={classes.emphasis}> EF Core </span>,
+                 <span className={classes.emphasis}> .Net Core </span> and <span className={classes.emphasis}> React </span>.
                 <br /><br />
-                At the same time, I am actively looking for a job. My aim is to become an excellent full-stack developer in the near future. 
-                Currently, I am looking for a job as a junior|graduate frontend|backend|full-stack developer. Depending on the opportunity, I am also 
-                open to internships or volunteering positions.
+                At the same time, <span className={classes.emphasis}> I am actively looking for a job </span>. My aim is to become an excellent <span className={classes.emphasis}> full-stack developer </span> in the near future. 
+                Currently, I am looking for a job as a <span className={classes.emphasis}>junior</span>|<span className={classes.emphasis}>graduate </span> <span className={classes.emphasis}>frontend</span>|<span className={classes.emphasis}>backend</span>|<span className={classes.emphasis}>full-stack </span> developer. Depending on the opportunity, I am also 
+                open to <span className={classes.emphasis}> internships </span> or <span className={classes.emphasis}> volunteering </span> positions.
                 <br /><br />
                 Thank you for reading thus far. I am looking forward to hearing from you!
               </div>  
